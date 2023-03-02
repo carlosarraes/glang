9debb98ac7c596733e93bcf754e62a2742bcacfe
carraes <carraeshb@gmail.com>
2023-03-01 23:32:33 -0300

    feat: main

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/carlosarraes/glang/cli"
+)
+
+var wg sync.WaitGroup
+
+var (
+	sourceLang string
+	targetLang string
+	sourceText string
+)
+
+func init() {
+	flag.StringVar(&sourceLang, "s", "en", "Source language[en]")
+	flag.StringVar(&targetLang, "t", "fr", "Target language[fr]")
+	flag.StringVar(&sourceText, "st", "", "Text to translate")
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		fmt.Println("No flags provided")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	strChan := make(chan string)
+
+	wg.Add(1)
+
+	reqBody := &cli.RequestBody{
+		SourceLang: sourceLang,
+		TargetLang: targetLang,
+		SourceText: sourceText,
+	}
+
+	go cli.RequestTranslate(reqBody, strChan, &wg)
+
+	processedStr := strings.ReplaceAll(<-strChan, "+", " ")
+
+	fmt.Printf("Translation: %s\n", processedStr)
+	close(strChan)
+
+	wg.Wait()
+}
